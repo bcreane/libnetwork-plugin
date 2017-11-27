991ca2a5ded9da2c103aeb190923f629a94ca9ed
Brendan Creane <brendan@tigera.io>
2017-11-27 12:47:29 -0800

    rev libcalico-go to v1.7.3.

--- driver/network_driver.go
+++ driver/network_driver.go
@@ -13,7 +13,6 @@ import (
 	libcalicoErrors "github.com/projectcalico/libcalico-go/lib/errors"
 	log "github.com/sirupsen/logrus"
 
-	dockerTypes "github.com/docker/docker/api/types"
 	dockerClient "github.com/docker/docker/client"
 	"github.com/docker/go-plugins-helpers/network"
 	"github.com/projectcalico/libcalico-go/lib/api"
@@ -271,7 +270,7 @@ func (d NetworkDriver) CreateEndpoint(request *network.CreateEndpointRequest) (*
 		return nil, err
 	}
 	defer dockerCli.Close()
-	networkData, err := dockerCli.NetworkInspect(context.Background(), request.NetworkID, dockerTypes.NetworkInspectOptions{})
+	networkData, err := dockerCli.NetworkInspect(context.Background(), request.NetworkID)
 	if err != nil {
 		err = errors.Wrapf(err, "Network %v inspection error", request.NetworkID)
 		log.Errorln(err)
@@ -486,7 +485,7 @@ RETRY_NETWORK_INSPECT:
 	}
 
 	// inspect our custom network
-	networkData, err := dockerCli.NetworkInspect(context.Background(), networkID, dockerTypes.NetworkInspectOptions{})
+	networkData, err := dockerCli.NetworkInspect(context.Background(), networkID)
 	if err != nil {
 		err = errors.Wrapf(err, "Error inspecting network %s - retrying (T=%s)", networkID, time.Since(start))
 		log.Warningln(err)
